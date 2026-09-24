01c57127d2622a26b5f3a0d320849bc01d9814c4
kashif ahmad <kashifahmad0047@gmail.com>
2026-09-24 01:47:24 -0700

    updated code

---
+++ assets/js/scripts.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("registerForm");
+
+  form.addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const response = await fetch("http://localhost:3000/api/users", {
+      method: "POST",
+
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  });
+});
